790f86e494670ac3165ef945df6606700275b3f0
Den <den.3.14ven@gmail.com>
2018-08-27 17:35:14 +0300

    problem solved

--- main.cpp
+++ main.cpp
@@ -13,64 +13,55 @@ using namespace std;
 int maxQAQ = 0;
 bool v[100] = { false };
 
-int dp_serach(int k, char* A,int n, bool st, bool midd) { // k - indx , n - size of A
-	if (st) {
-		//printf("%s", " st\n");
-		if (midd) {
-			//printf("%s", " midd\n");
-			for (int h = k; h < n; h++) {
-				if (A[h] == 'Q') {
-					maxQAQ++;	
-					/*if (h < n - 1 && !v[h]) {
-						v[h] = true;
-						int b = dp_serach(h + 1, A, n, true, false);
-					}
-					else return maxQAQ;*/
-				}
-			}
-		return maxQAQ;
-		}
-		else {
-			//printf("%s", " else midd\n");
-			for (int i = k; i < n; i++) {
-				if (A[i] == 'A') {
-					if (i < n - 1) {
-						int b = dp_serach(i + 1, A, n, true, true);
-					}
-					else return maxQAQ;
-				}
-			}
-			return maxQAQ;
-		}
-	}
-	else {
-		//printf("%s", " else st\n");
-		for (int i = k; i < n; i++) {
-			if (A[i] == 'Q' && !v[i]) {
-				v[i] = true;
-				if (i < n - 1) {
-					int b = dp_serach(i + 1, A, n, true, false);
-				}
-				else return maxQAQ;
-			}
-		}
-		return 0;
-	}
+int dp_serach(int k, char* A,int n, bool st, bool midd) { // k - index , n - size of A
+    if (st) {
+        if (midd) {
+            for (int h = k; h < n; h++) {
+                if (A[h] == 'Q') {
+                    maxQAQ++;
+                }
+            }
+            return maxQAQ;
+        }
+        else {
+            for (int i = k; i < n; i++) {
+                if (A[i] == 'A') {
+                    if (i < n - 1) {
+                        int b = dp_serach(i + 1, A, n, true, true);
+                    }
+                    else return maxQAQ;
+                }
+            }
+            return maxQAQ;
+        }
+    }
+    else {
+        for (int i = k; i < n; i++) {
+            if (A[i] == 'Q' && !v[i]) {
+                v[i] = true;
+                if (i < n - 1) {
+                    int b = dp_serach(i + 1, A, n, true, false);
+                }
+                else return maxQAQ;
+            }
+        }
+        return 0;
+    }
 
 }
 
 int main() {
-	int starting = 0, middle = 0, ending = 0;
-	char A[100];
-	int n = 100;
-	scanf("%s",A);
-	for (int i = 0; i < 100; i++) {
-		if ((int)A[i] > (int)'Z' || (int)'A' > (int)A[i]) {
-			n = i;
-			break;
-		}
-	}
-	int b = dp_serach(0, A, n, false, false);
-	printf("%d",maxQAQ);
-	return 0;
+    int starting = 0, middle = 0, ending = 0;
+    char A[100];
+    int n = 100;
+    scanf("%s",A);
+    for (int i = 0; i < 100; i++) {
+        if ((int)A[i] > (int)'Z' || (int)'A' > (int)A[i]) {
+            n = i;
+            break;
+        }
+    }
+    int b = dp_serach(0, A, n, false, false);
+    printf("%d",maxQAQ);
+    return 0;
 }
